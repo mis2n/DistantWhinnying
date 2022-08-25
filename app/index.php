0b1084138d46dde2edec97966f04b052eeaad402
John Ford <remoford@gmail.com>
2022-08-25 13:23:01 +0000

    first working container

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+HELLO THERE
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+//
+// // Show just the module information.
+// // phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+//
+?>
+
+
+</body>
+</html>
