227b9d77f6e6cc390f71e74c6dbf3514626a8d65
John Ford <remoford@gmail.com>
2022-08-25 16:46:06 +0000

    database connection works

--- app/index.php
+++ app/index.php
@@ -1,19 +1,35 @@
-<html>
+<!doctype html>
+<html lang=en>
+<head>
+	<meta charset=utf-8>
+</head>
 <body>
-
-HELLO THERE
-
+	<header></header>
+	<section>
+		<pre>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
-//
-// // Show just the module information.
-// // phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
-//
+echo "connecting to database\n";
+$db = new PDO('mysql:dbname=farm;host=localhost', 'admin', 'JmO2n6JENrsa');
+
+echo "showing tables\n";
+$query = $db->query('SHOW TABLES');
+$tables = $query->fetchAll(PDO::FETCH_COLUMN);
+foreach($tables as $table){
+	echo "Table: ";
+	print_r($table);
+	echo "\n";
+	$select = 'SELECT * FROM `' . $table . '`';
+	$rows = $db->query($select)->fetchALL(PDO::FETCH_ASSOC);
+	foreach($rows as $row)
+		print_r($row);
+	echo "\n";
+}
 ?>
-
-
+		</pre>
+	</section>
 </body>
 </html>
